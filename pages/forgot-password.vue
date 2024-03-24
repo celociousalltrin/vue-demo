452df21ef04d3edba45e92034c10273f8e22cdbe
Celocious Altrin <celocious@cloudpencils.com>
2024-03-24 22:09:05 +0530

    middleware and pages created

---
+++ pages/forgot-password.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <h1>Hi from {{ route.name }}</h1>
+  </div>
+</template>
+
+<script setup lang="ts">
+const route = useRoute();
+</script>
+
+<style scoped></style>
---
+++ pages/index.vue
@@ -0,0 +1,9 @@
+<template>
+  <div>
+    <h1>Hello from vue</h1>
+  </div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped></style>
